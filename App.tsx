7172e8c36317d1b64e0f2d9d2a20ed1c8d319c44
mohamadkhazaee <wallex@WALLEXs-MacBook-Air-9.local>
2023-07-27 01:14:07 +0330

    init

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Text, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="red" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    color: "black",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
